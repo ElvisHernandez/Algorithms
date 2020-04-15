705d8ad8e8f44fc3e1c6f570b94f7bd471b16a6c
ElvisHernandez <ElvisHernandezTheone@gmail.com>
2020-04-15 18:19:07 -0400

    created bisection function

--- bisection.c
+++ bisection.c
@@ -1,5 +1,17 @@
 #include <stdio.h>
 
+int binary_search(int nums[], int nums_length, int target);
+
 int main(void){
-	printf("Bisection");
+  int array[] = {11,7,-1,5,83,-13};
+  int target = -1;
+  binary_search(array, 6, target);
+}
+
+int binary_search(int nums[],int nums_length, int target){
+	int target_index = 0;
+    while (nums_length > 0) {
+        printf("nums_length: %i\n", nums_length);
+        nums_length--;
+    }
 }
