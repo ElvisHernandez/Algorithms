dc12f419c084b0e3856e692c1fc40c40f00bb7d6
ElvisHernandez <ElvisHernandezTheone@gmail.com>
2020-04-15 23:12:11 -0400

    finished fist iteration of the binary search function

--- bisection.c
+++ bisection.c
@@ -3,15 +3,29 @@
 int binary_search(int nums[], int nums_length, int target);
 
 int main(void){
-  int array[] = {11,7,-1,5,83,-13};
-  int target = -1;
-  binary_search(array, 6, target);
+  int array[] = {-77,-3,-1,0,4,13,29,81};
+  int target = 29;
+  binary_search(array, 8, target);
 }
 
 int binary_search(int nums[],int nums_length, int target){
-	int target_index = 0;
-    while (nums_length > 0) {
-        printf("nums_length: %i\n", nums_length);
-        nums_length--;
+    printf("The target value is: %i\n", target);
+    int start = 0;
+    int end = nums_length - 1;
+    int mid = (end-start) / 2;
+    while (end >= start) {
+        printf("These are the start, mid, and end points at the start of the loop: %i %i %i\n", start,mid,end);
+        if (nums[mid] == target) {
+        printf("This is the target array element: %i\n", nums[mid]);
+            return mid;
+        } else if (nums[mid] > target) {
+            end = mid - 1;
+        } else {
+            start = mid + 1;
+        }
+         mid = (start+end)/2;
+        printf("These are the start, mid, and end points at the end of the loop: %i %i %i\n", start,mid,end);
     }
+    printf("Youre target is not in the array. These were the ending start, mid, and end points %i %i %i\n,", start, mid, end );
+    return mid;
 }
