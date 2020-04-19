dc8c231ac953e45f2c4609414658032d862cbddc
ElvisHernandez <ElvisHernandezTheone@gmail.com>
2020-04-19 13:19:23 -0400

    fixed my insertion sort algorithm to be omega n and not omega n^2

--- insertion_sort.c
+++ insertion_sort.c
@@ -14,13 +14,11 @@ int main(void) {
 int insertion_sort(int nums[],int nums_length) {
     for (int i = 0; i < nums_length-1; i++) {
         int unsorted_element = nums[i+1];
-        int shifts = 0;
-        for (int j = i; j >= 0; j--) {
-            if (unsorted_element < nums[j]){
-                nums[j+1] = nums[j];
-                shifts++;
-            }
+        int j = i;
+        while (j >= 0 && nums[j] > unsorted_element) {
+            nums[j+1] = nums[j];
+            j--;
         }
-        nums[i+1-shifts] = unsorted_element;
+        nums[j+1] = unsorted_element;
     }
 }
